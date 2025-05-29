c82b0624753f1b6aab4353dbf0fbf44fca88d437
emanuelesguerzo <155169753+emanuelesguerzo@users.noreply.github.com>
2025-05-29 20:45:37 +0200

    feat: add project image display in card

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,9 +1,18 @@
 import { Link } from 'react-router-dom'
 
+const api = import.meta.env.VITE_API_URL;
+
 function ProjectCard({ project }) {
+
     return (
         <div className="card h-100">
-            <img src={`/public/${project.cover_image}`} alt={project.title} />
+            {project.cover_image && (
+                <img
+                    src={`${api}/storage/${project.cover_image}`}
+                    alt={`Immagine del progetto ${project.title}`}
+                    style={{ width: '100%', height: '200px', objectFit: 'cover', objectPosition: 'top' }}
+                />
+            )}
             <h2>{project.title}</h2>
             <p><strong>Tipo:</strong> {project.type.name}</p>
             <a href={project.repo_url} target="_blank" rel="noopener noreferrer">
